69aeccba90c7a2d510d38466c926347bdcd6bca1
Nina Yordanova <nina.yordanova@xentio.com>
2015-01-18 23:25:01 -0800

    sd

--- app/src/main/assets/www/js/main.js
+++ app/src/main/assets/www/js/main.js
@@ -1,12 +1,30 @@
 $(function(){
+  localStorage.username = "";
   $( "#loginBtn" ).bind( "tap", submitLogin );
 
   function submitLogin( event ){
     localStorage.mail     = $('#mail').val();
     localStorage.username = $('#username').val();
     localStorage.password = $('#password').val();
-    $.mobile.changePage("#home");
-  }
 
-  $( ".username" ).text( localStorage.username );
+    if(localStorage.mail != "" && localStorage.username != "" && localStorage.password != ""){
+      $.mobile.changePage("#home");
+      var lat = navigator.geolocation.getCurrentPosition(geolocationOnSuccess, geolocationOnError);
+      $( ".username" ).text( localStorage.username + " " + lat);
+
+    }
+    else {
+      $( ".error" ).text( "Ппопълнете полетата" );
+    }
+  };
+  function geolocationOnSuccess (position) {
+    alert("OK");
+    alert(position.coords.latitude,position.coords.longitude);
+    return position.coords.latitude;
+  };
+  function geolocationOnError (error) {
+    alert("ERROR");
+    alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+    return error;
+  };
 });
